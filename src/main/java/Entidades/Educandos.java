4255fcf8b1be6a573cd816289c19e6f606c386b0
rafaelmartincarrion <33486144+rafaelmartincarrion@users.noreply.github.com>
2018-05-10 13:35:59 +0200

    Cambio de nombre a Entidades
    He cambiado el nombre del paquete donde se encuentran las entidades para poder crear los paquetes del backend(se llamaba scouts)

---
+++ src/main/java/Entidades/Educandos.java
@@ -0,0 +1,45 @@
+package Entidades;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+
+/**
+ *
+ * @author Rafael Martin Carrion, Joaquin Terrasa Moya
+ */
+@Entity
+@DiscriminatorValue("Edu")
+public class Educandos extends Usuario implements Serializable {
+        
+    private static final long serialVersionUID = 1L;
+
+    //! Claves foraneas
+    
+    
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Grupo grupoEducandos;
+
+    @OneToMany(mappedBy = "Educando")
+    @JoinColumn(nullable = false)
+    private List<Facturas_Clientes> facturas;
+    
+    
+    //! Getters y Setters
+    
+    
+    public Grupo getGrupo() {
+        return grupoEducandos;
+    }
+
+    public void setGrupo(Grupo grupoEducandos) {
+        this.grupoEducandos = grupoEducandos;
+    }
+        
+}
