2304733f5277222071b40e8910e3f5f0fa267cfc
Quino Terrasa <quino.terrasa@gmail.com>
2018-05-12 19:08:24 +0200

    Creada clase infoSession
    Esta clase mantiene el objeto Usuario a traves de toda la sesion.

--- src/main/java/backendLogin/InfoSession.java
+++ src/main/java/backendLogin/InfoSession.java
@@ -5,10 +5,60 @@
  */
 package backendLogin;
 
+import Entidades.Usuario;
+import java.io.Serializable;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
 /**
  *
  * @author flamenquim
  */
-public class InfoSession {
+@Named(value = "infoSession")
+@SessionScoped
+public class InfoSession implements Serializable {
     
+    private Usuario usuario;
+    
+    //! Constructor
+    
+    public InfoSession() {
+        
+    }
+    
+    //! Getter, setter
+    
+    public Usuario getUsuario() { return usuario; }
+    
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    //! Metodos
+    
+    public String inicio() {
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        
+        if(usuario != null) {
+            //! Redirecciona a la pagina de inicio dependiendo del rol
+            switch(usuario.getRolUsuario()) {
+                case "Educando": return "PrincipalEducando.xhtml";
+                case "Admin": return "PrincipalAdmin.xhtml"; 
+                default: return "PrincipalScouter.xhtml";
+            }
+            
+        } else { 
+            return null;
+        }
+    }
+    
+    public String invalidarSesion() {
+        // Destruye la sesión (y con ello, el ámbito de este bean)
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        ctx.getExternalContext().invalidateSession();
+        usuario = null;
+        
+        return "login.xhtml";
+    }
 }
